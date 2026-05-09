eca2e8ddf4a63fa502ae29205d296e5b2378b14b
dija-hub <213936136+dija-hub@users.noreply.github.com>
2026-05-09 22:37:56 +0500

    sign in

--- quotonic/sign.js
+++ quotonic/sign.js
@@ -1,10 +1,43 @@
-let email=getElementById("info")
-let pass=getElementById("password")
-let msg=getElementById("msg")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
- if(email===password){
-    msg.innerText="Login succssfully"
- }else{
-    msg.innerText="Try Again!"
- }
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFJmTWLJSmkQQJZPgN0r5xvO_v9wmpgNQ",
+  authDomain: "habbit-tracker-24d3c.firebaseapp.com",
+  projectId: "habbit-tracker-24d3c",
+  storageBucket: "habbit-tracker-24d3c.firebasestorage.app",
+  messagingSenderId: "1069162891610",
+  appId: "1:1069162891610:web:5cf736697e3a0fde170b63"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+let email = document.getElementById("info");
+let pass = document.getElementById("pass");
+let msg = document.getElementById("msg");
+let enter = document.getElementById("btn3");
+
+
+enter.addEventListener("click", function () {
+
+  signInWithEmailAndPassword(auth, email.value, pass.value)
+
+    .then(() => {
+      msg.innerText = "Sign In Successful 🚀";
+      
+    })
+
+    .catch((error) => {
+      msg.innerText = "Wrong Email or Password ❌";
+      
+    });
+
+});
