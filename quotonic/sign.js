9b45347c62207085ff514b486a80079c74fdac3b
dija-hub <213936136+dija-hub@users.noreply.github.com>
2026-05-09 23:02:21 +0500

    sign in

--- quotonic/sign.js
+++ quotonic/sign.js
@@ -26,18 +26,15 @@ let msg = document.getElementById("msg");
 let enter = document.getElementById("btn3");
 
 
-enter.addEventListener("click", function () {
+enter.addEventListener("click",function(){
 
-  signInWithEmailAndPassword(auth, email.value, pass.value)
-
-    .then(() => {
-      msg.innerText = "Sign In Successful 🚀";
-      
-    })
-
-    .catch((error) => {
-      msg.innerText = "Wrong Email or Password ❌";
-      
-    });
-
-});
+  signInWithEmailAndPassword(auth,email.value,pass.value)
+  .then(()=>{
+       msg.innerText="Sign In Successful"
+       msg.style.color = "green";
+  })
+  .catch(()=>{
+    msg.innerText="Wrong Password or Email"
+    msg.style.color = "red";
+  })
+})
