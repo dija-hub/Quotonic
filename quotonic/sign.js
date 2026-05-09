5b2154b9ff784de12298bf6835813a149a82ccea
dija-hub <213936136+dija-hub@users.noreply.github.com>
2026-05-09 17:09:20 +0500

    sign in

--- quotonic/sign.js
+++ quotonic/sign.js
@@ -1,4 +1,10 @@
 let email=getElementById("info")
 let pass=getElementById("password")
+let msg=getElementById("msg")
 
+ if(email===password){
+    msg.innerText="Login succssfully"
+ }else{
+    msg.innerText="Try Again!"
+ }
 
