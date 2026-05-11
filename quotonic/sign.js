e08c577e3fa3a505e85eabb1b8cd93ecc54e2b22
dija-hub <213936136+dija-hub@users.noreply.github.com>
2026-05-11 20:43:48 +0500

    features

--- quotonic/sign.js
+++ quotonic/sign.js
@@ -8,19 +8,23 @@ let msg = document.getElementById("msg");
 let enter = document.getElementById("btn3");
 
 
-enter.addEventListener("click", async function () {
+// enter.addEventListener("click", async function () {
 
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email: email.value,
-    password: pass.value
-  });
+//   const { data, error } = await supabase.auth.signInWithPassword({
+//     email: email.value,
+//     password: pass.value
+//   });
 
-  if (error) {
-    msg.innerText = "Wrong Email or Password";
-    msg.style.color = "red";
-  } else {
-    msg.innerText = "Sign In Successful";
-    msg.style.color = "green";
-  }
+//   if (error) {
+//     msg.innerText = "Wrong Email or Password";
+//     msg.style.color = "red";
+//   } else {
+//     msg.innerText = "Sign In Successful";
+//     msg.style.color = "green";
+//   }
 
-});
+// });
+
+enter.addEventListener('click',await function(){
+  
+})
